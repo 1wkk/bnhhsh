b942c766e7856b9609eb2bb8e814eccc9d6fa6d4
ryanmoyo <iherewithmyheart@gmail.com>
2022-04-12 19:45:01 +0800

    ✅ vitest

--- __test__/test.js
+++ __test__/test.js
@@ -1,3 +1,19 @@
 import dp from '../dist/bnhhsh'
+// import { test, expect } from 'vitest'
 
-console.log(dp('ynmm'))
+// test('bnhhsh', () => {
+//   expect(dp('ynmm')).toBe('幼女魅魔')
+//   expect(
+//     dp(
+//       'cfsiisetsacobhnwovafxsarlyioxbfsequinnoyxxmpgvfzmocdqxiqmzhmtymivlclfeuvcoxzyztnatwvsbqznjxgcqgibsqxepqfglbmdnjwjzzjdwqhzquirqmjptpgs'
+//     )
+//   ).toBe(
+//     '草风骚爱爱沙俄桃⾊俺草欧屄好嫩玩偶v俺粉⽳骚爱⼈狼友爱偶像屄风骚⼉亲幼爱嫩⼥欧淫亵学妹屁股v反正摸欧草屌强⾏爱亲妹⼦好美腿淫媚爱v轮操浪妇⼉幼v草欧写真应召套弄俺体位v⾊逼亲中年菊⽳⾼潮⽋⼲爱屄⾊区⽳饿殍亲反感浪逼摸荡⼥接吻精⼦招妓屌⽆穷合租亲幼爱⼈妻迷奸啪偷拍⼲死'
+//   )
+// })
+
+console.log(
+  dp(
+    'cfsiisetsacobhnwovafxsarlyioxbfsequinnoyxxmpgvfzmocdqxiqmzhmtymivlclfeuvcoxzyztnatwvsbqznjxgcqgibsqxepqfglbmdnjwjzzjdwqhzquirqmjptpgs'
+  )
+)
--- lib/bnhhsh.spec.ts
+++ lib/bnhhsh.spec.ts
@@ -1,3 +1,13 @@
 import dp from './bnhhsh'
+import { test, expect } from 'vitest'
 
-console.log(dp('ynmm'))
+test('bnhhsh', () => {
+  expect(dp('ynmm')).toBe('幼女魅魔')
+  expect(
+    dp(
+      'cfsiisetsacobhnwovafxsarlyioxbfsequinnoyxxmpgvfzmocdqxiqmzhmtymivlclfeuvcoxzyztnatwvsbqznjxgcqgibsqxepqfglbmdnjwjzzjdwqhzquirqmjptpgs'
+    )
+  ).toMatch(
+    '草风骚爱爱沙俄桃色俺草欧屄好嫩玩偶v俺粉穴骚爱人狼友爱偶像屄风骚儿亲幼爱嫩女欧淫亵学妹屁股v反正摸欧草屌亲穴爱亲妹子好美腿淫媚爱v轮操浪妇儿幼v草欧写真应召套弄俺体位v色逼亲中年菊穴高潮欠干爱屄色区穴饿殍亲反感浪逼摸荡女接吻精子招妓屌无穷合租亲幼爱人妻迷奸啪偷拍干死'
+  )
+})
